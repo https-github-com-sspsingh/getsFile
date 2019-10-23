a4234243a3e5c8b1e8497561d36f179292c7e1e5
Sukhwinder Singh <sukhwinder.singh@webchilli.com>
2019-10-23 12:48:42 +0530

    Add files via upload

--- 505.php
+++ 505.php
@@ -17,7 +17,7 @@ include 'sidebar.php';
 			<div class="error-content">
 				<h3><i class="fa fa-warning text-yellow"></i> Oops! Something went wrong.</h3>
 				<p>
-					We will work on fixing that right away. 
+					We will work on fixing that right away. <br />  <br /> 
 					Meanwhile, you may <a href='../../index.html'>return to dashboard</a> or try using the search form.
 				</p>
 				<form class='search-form'>
